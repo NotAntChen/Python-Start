50f69b01f00a34f554b20be48444a9448e9b979b
NotAntChen <111203636+NotAntChen@users.noreply.github.com>
2024-05-30 21:24:37 -0400

    comments to twosumassignment.py

--- DSA/twosumassignment.py
+++ DSA/twosumassignment.py
@@ -1,11 +1,14 @@
-import array
-
 def twosum(inputarr,target):
+    #initialize output array with zeroes for now
     outputarr = [0,0]
+    #for n elements in the input array
     for i in range(len(inputarr)):
+        #sum the i-th element with another element inside of the array
         for j in range(i+1,len(inputarr)):
+            #if sum of the two elements matches the target, return an output array that has the indices of the two numbers
             if inputarr[i] + inputarr[j] == target:
-                outputarr = [i,j]
+                outputarr[0] = i
+                outputarr[1] = j
                 return outputarr
 
 input = [2,7,11,15]
