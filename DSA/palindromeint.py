264fb6eb6e246b07be3d8fdd7344b9722a5ef98f
NotAntChen <111203636+NotAntChen@users.noreply.github.com>
2024-06-02 12:01:11 -0400

    palindrome without using strings

---
+++ DSA/palindromeint.py
@@ -0,0 +1,23 @@
+def palidromeint(x: int) -> bool:
+    y = 0
+    og = x
+    if x<0:
+        #if negative, it is not a palindrome
+        return False
+    elif x % 10 == 0 and x != 0:
+        #if it ends in 0 and 2 or more digitsm it is not a palindrome
+        return False
+    while x > 0:
+        r = x % 10
+        y = y*10 + r
+        x = x//10
+        if y == og:
+            return True
+
+
+x = 121
+print(palidromeint(x))
+x = -121
+print(palidromeint(x))
+x = 10
+print(palidromeint(x))
